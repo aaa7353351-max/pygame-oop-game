5d0a3664e2fe8166601fc9c59b1b70ee4ec827d1
nje <aaa7353351@gmail.com>
2025-11-11 23:27:41 +0900

    기획 주석 추가

--- scr/main.py
+++ scr/main.py
@@ -23,10 +23,23 @@ font = pygame.font.Font(None,40)
 WHITE = (255,255,255)
 
 # 클래스 정의
+
+#클래스 Background / 배경그림관리 draw()
 class Background:
     def __init__(self, img_path):
         self.image =  pygame.image.load(img_path).convert() #이미지 로드 후 최적화
         self.image = pygame.transform.scale(self.image, (WIDTH,HEIGHT)) #크기변환  
+
+
+
+
+#클래스 Player / 조작 & hp관리 move(), draw()
+
+
+
+# . . .클래스들 정리해두고 커밋 > 게임관리 클래스 Game 만들고 커밋 > 게임루프 run 구성후 커밋 >게임종료처리
+
+
 
 #종료 
 pygame.quit()
