074441f43a07a8bf77777d0a79acff9027957c0b
노정은 <njo3606@naver.com>
2025-11-12 17:51:32 +0900

    게임 루프

--- main.py
+++ main.py
@@ -74,14 +74,85 @@ class Item:
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
+# ----게임 클래스 
+class Game:
+    def __init__(self):
+        self.bg = Background("background.png")
+        self.player = Player("bird.png")
+        self.enemies = [Enemy("enemy.png") for _ in range(3)]
+        self.items = [Item("item.png")]
+        self.start_time = pygame.time.get_ticks()
+        self.running = True
+    def check_collision(self):
+       # 적 충돌
+        for e in self.enemies:
+            if self.player.rect.colliderect(e.rect):
+                self.player.hp -= 1
+                e.rect.x = WIDTH + random.randint(100, 300)
+                if self.player.hp <= 0:
+                    self.running = False
+        # 아이템 충돌
+        for i in self.items:
+            if self.player.rect.colliderect(i.rect):
+                self.player.hp += 1
+                i.rect.x = WIDTH + random.randint(300, 500)
+
+#---- 게임루프구성
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            keys = pygame.key.get_pressed()
+            self.player.move(keys)
+            # 업데이트
+            for e in self.enemies:
+                e.update()
+            for i in self.items:
+                i.update()
+            self.check_collision()
+            # 점수 계산
+            elapsed_time = (pygame.time.get_ticks() - self.start_time) // 1000
+            # 화면 그리기
+            self.bg.draw(screen)
+            self.player.draw(screen)
+            for e in self.enemies:
+                e.draw(screen)
+            for i in self.items:
+                i.draw(screen)
+            # 정보 표시
+            text = font.render(f"HP: {self.player.hp}   Time: {elapsed_time}", True, WHITE)
+            screen.blit(text, (10, 10))
+            pygame.display.flip()
+            clock.tick(60)
+        # 게임 종료 화면
+        screen.fill((0, 0, 0))
+        gameover_text = font.render(f"Game Over! Score: {elapsed_time}", True, WHITE)
+        screen.blit(gameover_text, (WIDTH//2 - 150, HEIGHT//2))
+        pygame.display.flip()
+        pygame.time.wait(2000)
+# --- 실행 --- #
+if __name__ == "__main__":
+    Game().run()
 
 
 
 
 
-# . . .클래스들 정리해두고 커밋 > 게임관리 클래스 Game 만들고 커밋 > 게임루프 run 구성후 커밋 >게임종료처리
 
 
+
+
+
+
+
+
+
+
+# . . .클래스들 정리해두고 커밋(완) > 게임관리 클래스 Game 만들고 커밋 > 게임루프 run 구성후 커밋 >게임종료처리
+
+# 추가해야할 이미지 배경, 적, 그리고 브금 설정하기
 
 #종료 
 pygame.quit()
